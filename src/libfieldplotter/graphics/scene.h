e4ce3b7fbd65c84cde1289440b1a3ea16b6173eb
jkotrc <john.kotrc@gmail.com>
2021-05-22 16:12:35 +0200

    started making the scene unit test

--- src/libfieldplotter/graphics/scene.h
+++ src/libfieldplotter/graphics/scene.h
@@ -3,7 +3,8 @@
 
 /*
 ** A scene object to which you can add individual components
-** for drawing.
+** for drawing. Each scene should have its own screen (middle layer delegate to the framebuffer)
 */
+
 
 #endif // __SCENE_H_
